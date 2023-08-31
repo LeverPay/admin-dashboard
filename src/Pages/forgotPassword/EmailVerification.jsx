2959907d1f99ca89c46125ab98efbe9a977dbfac
Godswill Udoh <Godswill.Udoh@sterling.ng>
2023-08-31 12:39:43 +0100

    revert to old design

---
+++ src/Pages/forgotPassword/EmailVerification.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import logo from '../../assets/LeverPay Gold.png';
+import { useNavigate } from 'react-router-dom';
+
+const EmailVerification = () => {
+  const navigate = useNavigate();
+  const navigateToEmailVerification = () => {
+    navigate('/email-verification');
+  };
+  return (
+    <div className="bg-white w-full text-[#0F261D ] p-5">
+      <div className="w-[150px] h-auto">
+        <img src={logo} alt="leverpay logo" />
+      </div>
+
+      <div className="flex items-center justify-center h-[90vh] flex-col space-y-3">
+        <h2 className="text-gray-800 text-2xl font-bold">Email Verification</h2>
+        <p className="text-gray-800 text-xl font-normal leading-normal">
+          Please enter the 6-digit code that was sent to your email{' '}
+        </p>
+
+        <div className="flex items-center justify-center gap-5">
+          <input className="w-14 h-14 px-2.5 rounded-sm border border-gray-800 justify-start items-center gap-6 inline-flex" />
+          <input className="w-14 h-14 px-2.5 rounded-sm border border-gray-800 justify-start items-center gap-6 inline-flex" />
+          <input className="w-14 h-14 px-2.5 rounded-sm border border-gray-800 justify-start items-center gap-6 inline-flex" />
+          <input className="w-14 h-14 px-2.5 rounded-sm border border-gray-800 justify-start items-center gap-6 inline-flex" />
+          <input className="w-14 h-14 px-2.5 rounded-sm border border-gray-800 justify-start items-center gap-6 inline-flex" />
+          <input className="w-14 h-14 px-2.5 rounded-sm border border-gray-800 justify-start items-center gap-6 inline-flex" />
+        </div>
+
+        <div
+          className="w-[568px] h-14 p-2.5 my-5 text-center bg-blue-600 rounded-[10px] justify-center cursor-pointer items-center gap-2.5 flex"
+          onClick={navigateToEmailVerification}
+        >
+          <div className="text-white text-lg font-bold leading-normal">
+            {' '}
+            Verify email address{' '}
+          </div>
+        </div>
+
+        <div>
+          <span className="text-gray-800 text-base font-normal leading-tight">
+            Didn’t get a code?{' '}
+          </span>
+          <span className="text-blue-600 text-base font-bold underline leading-tight">
+            Resend
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmailVerification;
---
+++ src/Pages/forgotPassword/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import logo from '../../assets/LeverPay Gold.png';
+import { useNavigate } from 'react-router-dom';
+
+const ForgotPassword = () => {
+  const navigate = useNavigate();
+  const navigateToEmailVerification = () => {
+    navigate('/email-verification');
+  };
+  return (
+    <div className="bg-white w-full text-[#0F261D ] p-5">
+      <div className="w-[150px] h-auto">
+        <img src={logo} alt="leverpay logo" />
+      </div>
+
+      <div className="flex items-center justify-center h-[90vh] flex-col">
+        <h2 className="text-center text-green-950 text-[40px] font-normal leading-normal">
+          Forgot Password
+        </h2>
+        <p className="w-[430px] h-[23px] text-gray-800 text-sm font-normal leading-none">
+          Please enter the email address used to register with lever pay
+        </p>
+
+        <input
+          className="w-[568px] h-14 px-2.5 rounded-[10px] border border-gray-800 border-opacity-20 justify-between items-center gap-[227px] inline-flex text-gray-800 text-opacity-50 text-base font-normal leading-tight outline-none"
+          placeholder="Enter your email ID"
+        />
+
+        <div
+          className="w-[568px] h-14 p-2.5 my-5 text-center bg-blue-600 rounded-[10px] justify-center cursor-pointer items-center gap-2.5 flex"
+          onClick={navigateToEmailVerification}
+        >
+          <div className="text-white text-lg font-bold leading-normal">
+            {' '}
+            Reset Password
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPassword;
